19a6527df409940e203572f31c955b521ebcccaa
Tytus Harlender <tytus.harlender95@gmail.com>
2023-07-19 23:38:36 +0200

    [master] - formatiing HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,21 @@ using System.Diagnostics;
 namespace _19_07_2023_task1.Controllers
 {
     public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    {
+        private readonly ILogger<HomeController> _logger;
 
-		private readonly string _baseUrl = "https://wl-api.mf.gov.pl/api/search/nip/";
+        private readonly string _baseUrl = "https://wl-api.mf.gov.pl/api/search/nip/";
 
-		private static HttpClient _httpClient = new HttpClient();
+        private static HttpClient _httpClient = new HttpClient();
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
         public IActionResult Index()
         {
-            
+
             return View();
         }
 
@@ -32,28 +32,28 @@ namespace _19_07_2023_task1.Controllers
         }
 
         [HttpPost]
-		public async Task<IActionResult> Search(SearchViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var currentDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
+        public async Task<IActionResult> Search(SearchViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
                 _httpClient.BaseAddress = new Uri(_baseUrl);
-				_httpClient.DefaultRequestHeaders.Clear();
-				var response2 = await _httpClient.GetFromJsonAsync<Root>($"{_baseUrl}{model.Nip}?date={currentDate}");
-				return View();
-			}
-			return View();
-		}
+                _httpClient.DefaultRequestHeaders.Clear();
+                var response2 = await _httpClient.GetFromJsonAsync<Root>($"{_baseUrl}{model.Nip}?date={currentDate}");
+                return View();
+            }
+            return View();
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
