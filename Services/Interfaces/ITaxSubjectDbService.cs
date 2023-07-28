033598268dea98874a84527288789fcd1d700196
Tytus Harlender <tytus.harlender95@gmail.com>
2023-07-28 18:13:00 +0200

    master- adding service for handling DB requests to repository

---
+++ Services/Interfaces/ITaxSubjectDbService.cs
@@ -0,0 +1,9 @@
+﻿using _19_07_2023_task1.Models;
+
+namespace _19_07_2023_task1.Services.Interfaces
+{
+	public interface ITaxSubjectDbService
+	{
+		public void SaveReceivedTaxSubjectData(ReceivedSubject data);
+	}
+}
---
+++ Services/TaxSubjectDbService.cs
@@ -0,0 +1,20 @@
+﻿using _19_07_2023_task1.Data.Repositories.Interfaces;
+using _19_07_2023_task1.Models;
+using _19_07_2023_task1.Services.Interfaces;
+
+namespace _19_07_2023_task1.Services
+{
+	public class TaxSubjectDbService : ITaxSubjectDbService
+	{
+		private readonly ITaxDataSearchRepository _taxDataSearchRepository;
+
+		public TaxSubjectDbService(ITaxDataSearchRepository taxDataSearchRepository)
+        {
+			_taxDataSearchRepository = taxDataSearchRepository;
+		}
+        public void SaveReceivedTaxSubjectData(ReceivedSubject data)
+		{
+			_taxDataSearchRepository.SaveTaxSubjectData(data);
+		}
+	}
+}
