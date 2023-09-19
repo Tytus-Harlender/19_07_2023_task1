769232ca581d3676c13ebd7a9da5ce4b4218fd29
Tytus Harlender <tytus.harlender95@gmail.com>
2023-09-19 11:51:45 +0200

    adding validation to the search box

--- Services/TaxSubjectApiCaller.cs
+++ Services/TaxSubjectApiCaller.cs
@@ -6,18 +6,16 @@ namespace _19_07_2023_task1.Services
 {
     public class TaxSubjectApiCaller : ITaxSubjectsApiCaller
 	{
-		private static HttpClient _httpClient = new HttpClient();
 
-		private readonly string _baseUrl = "https://wl-test.mf.gov.pl/api/search/nip/";
+		private static HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("https://wl-test.mf.gov.pl/api/search/nip/") };
 
-		public async Task<string> GetStringResponseByTINAsync(SearchViewModel model)
+        public async Task<string> GetStringResponseByTINAsync(SearchViewModel model)
 		{
 			try
 			{
 				var currentDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
-				_httpClient.BaseAddress = new Uri(_baseUrl);
 				_httpClient.DefaultRequestHeaders.Clear();
-				var response = await _httpClient.GetStringAsync($"{_baseUrl}{model.Nip}?date={currentDate}");
+				var response = await _httpClient.GetStringAsync($"{_httpClient.BaseAddress}{model.Nip}?date={currentDate}");
 				return response;
 			}
 			catch (Exception ex)
@@ -32,14 +30,13 @@ namespace _19_07_2023_task1.Services
 			try
 			{
 				var currentDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
-				_httpClient.BaseAddress = new Uri(_baseUrl);
 				_httpClient.DefaultRequestHeaders.Clear();
-				var response = await _httpClient.GetFromJsonAsync<Root>($"{_baseUrl}{model.Nip}?date={currentDate}");
+				var response = await _httpClient.GetFromJsonAsync<Root>($"{_httpClient.BaseAddress}{model.Nip}?date={currentDate}");
 				model.Root = response;
 			}
 			catch(Exception ex)
 			{
-				Console.WriteLine("Unhandled exception: " + ex.Message);
+				Console.WriteLine("Unhandled exception during API call: " + ex.Message);
 			}
 			return Task.FromResult(model).Result;
 		}
